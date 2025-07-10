0e3305769a83c7a621110ea809c800740649937a
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-07-10 10:27:37 +0300

    123

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(1)
+import { Server } from "socket.io";
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.static(__dirname + "/public/"))
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
