fe90b0e9c43350979a47f1b4693b58673cd48de6
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-07-10 11:49:34 +0300

    chat

--- app.js
+++ app.js
@@ -10,19 +10,34 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  connectionStateRecovery: {}
+    connectionStateRecovery: {}
 });
+
+
+const room = "123456789"
 
 
 app.use(express.static(__dirname + "/public/"))
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
+    console.log('a user connected');
+    
+
+    socket.on("message", (data) => {
+        console.log(data)
+        socket.join(data.room);
+        io.to(data.room).emit("message", {
+            message: data.message
+        })
+
+    })
 });
 
+
+
+
+
+
 server.listen(3000, '0.0.0.0', () => {
-  console.log('server running at http://localhost:3000');
+    console.log('server running at http://localhost:3000');
 });
--- public/index.js
+++ public/index.js
@@ -1,12 +1,28 @@
 const socket = io();
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-
+const messagesList = document.getElementById("messages")
+const room = "123456789"
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     if (input.value) {
-        socket.emit('chat message', input.value);
+
+        socket.emit("message", {
+            message: input.value,
+            roomId: room
+        });
         input.value = '';
     }
 });
+
+
+socket.on("message", (data)=>{
+     console.log(data.message)
+
+     const msg = document.createElement("li")
+
+     msg.textContent = data.message
+     messagesList.appendChild(msg)
+})
