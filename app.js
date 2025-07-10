595a772c415e4669059965bd18240c64cfb4b056
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-07-10 10:52:13 +0300

    default

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
+
 
 app.use(express.static(__dirname + "/public/"))
 
-server.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+   socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(3000, '0.0.0.0', () => {
   console.log('server running at http://localhost:3000');
 });
