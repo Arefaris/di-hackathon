9442b4bdb5cc366e6b619dca089781dd3eea2fda
Arefaris <arefaris6969@gmail.com>
2025-07-11 11:10:40 +0300

    chat-front-style

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,6 +1,6 @@
 export function handleChatEvents(io, socket) {
     socket.on('message', ({ message, roomID }) => {
-
+        console.log(message, roomID)
         if (!roomID || !message) return;
         console.log(`${socket.id} is writing in room ${roomID}: ${message}`); //TODO delete in prod
 
