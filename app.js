b99e73538c0a24e09969b7b3fd8f8e5a88c91e35
platovich <platovich@yandex.ru>
2025-07-12 22:02:10 +0300

    Added essential modules for server runtime.

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'node:url';
@@ -6,14 +11,31 @@ import { dirname, join } from 'node:path';
 import { setupSocket } from './socket/index.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // not more than 100 requests 
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(morgan('dev'));
+app.use(limiter);
+
+// Base error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
 const server = createServer(app);
 const io = new Server(server, {
   connectionStateRecovery: {},
 });
-
 app.use(express.static(join(__dirname, "public")));
-
 setupSocket(io);
 
 server.listen(3000, '0.0.0.0', () => {
