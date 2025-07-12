0305bae8134a5d310f8d237ec7fc4efb5c468a66
platovich <platovich@yandex.ru>
2025-07-12 23:41:11 +0300

    Refactoring users API. Added routes for users.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,5 @@
 import { createMessage } from '../models/messageModel.js'
-import { findUserByUsername } from '../models/userModel.js'
+import { getUserByUsername } from '../models/userModel.js'
 import { getChatByGUID } from '../models/chatModel.js'
 
 export function handleChatEvents(io, socket) {
@@ -8,7 +8,7 @@ export function handleChatEvents(io, socket) {
         if (!roomID || !message || !sender) return;
         console.log(`${sender} is writing in room ${roomID}: ${message}`); //TODO delete in prod
         const chat =  await getChatByGUID(roomID);;
-        const user = await findUserByUsername(sender);
+        const user = await getUserByUsername(sender);
         await createMessage({chat_id: chat.id, sender_id: user.id, content: message})
         socket.join(roomID);
         io.to(roomID).emit('message', {
