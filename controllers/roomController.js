b4f86c14ac793fed3fd5c93b375c207c52489d34
platovich <platovich@yandex.ru>
2025-07-12 10:50:18 +0300

    Save messages to DB

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -3,13 +3,18 @@ import { createChat } from '../models/chatModel.js';
 import { addParticipant } from '../models/chatParticipantModel.js'
 import { getChatByGUID } from '../models/chatModel.js'
 import { getMessagesForChat } from '../models/messageModel.js'
+import { createUser } from '../models/userModel.js'
 
 export function handleRoomEvents(io, socket) {
   socket.on('create', async ({ nickname, roomname }) => {
     try {
       const roomID = uuidv4();
       const [chat] = await createChat({ name: roomname, type: 'group', guid: roomID });
-      const user_id = 1;// TODO: rebuild getting user_id as socket.handshake.auth.user_id;
+
+      //Create user by nickname
+
+      const user = await createUser({ username: nickname, password_hash: 'passHash#1' });
+      const user_id = user.id;// TODO: rebuild getting user_id as socket.handshake.auth.user_id;
       if (user_id) {
         await addParticipant({ chat_id: chat.id, user_id });
       }
