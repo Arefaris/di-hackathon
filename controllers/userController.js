b89bd72918968c731dc6f1ed0bc0ea495d045c45
platovich <platovich@yandex.ru>
2025-07-27 23:20:06 +0300

    Store IP and user_agent info on creation auth token

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,18 +20,19 @@ const signToken = (id, secret, expiresIn) => {
 
 // Function to create and send JWT tokens
 // This function generates both access and refresh tokens, stores them in cookies, and sends them in the response
-const createSendToken = async (user, statusCode, res) => {
+const createSendToken = async (user, statusCode, req, res) => {
     const accessToken = signToken(user.id, process.env.JWT_SECRET, process.env.JWT_EXPIRES_IN);
     const refreshToken = signToken(user.id, process.env.JWT_REFRESH_SECRET, process.env.JWT_REFRESH_EXPIRES_IN);
-
-  
-    // await updateUserById(user.id, { refresh_token: refreshToken });
+    // Get user agent and IP address from request headers
+    const user_agent = req.headers['user-agent'] || req.ip || 'Unknown';
+    const ip_address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // Store the refresh token in the database
     await createRefreshToken({
         user_id: user.id,
         token: refreshToken,
         expires_at: new Date(Date.now() + process.env.JWT_REFRESH_EXPIRES_IN.slice(0, -1) * 24 * 60 * 60 * 1000),
-        user_agent: null, 
-        ip_address: null 
+        user_agent,
+        ip_address
     });
 
     // Set cookies for access token
@@ -39,13 +40,12 @@ const createSendToken = async (user, statusCode, res) => {
         expires: new Date(Date.now() + process.env.JWT_EXPIRES_IN.slice(0, -1) * 60 * 60 * 1000), // 1 hour
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // HTTP only for security in production
-        sameSite: 'Lax', 
+        sameSite: 'Lax',
     };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
     // Set cookies for refresh token
     res.cookie('jwt', accessToken, cookieOptions);
-    res.cookie('refreshJwt', refreshToken, { 
+    res.cookie('refreshJwt', refreshToken, {
         expires: new Date(Date.now() + process.env.JWT_REFRESH_EXPIRES_IN.slice(0, -1) * 24 * 60 * 60 * 1000),  // 7 days
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
@@ -53,13 +53,12 @@ const createSendToken = async (user, statusCode, res) => {
     });
 
 
-    // Убираем хэш пароля из ответа
-    user.password_hash = undefined; // Важно!
-
+    // Remove sensitive data from user object
+    const { password_hash, ...safeUser } = user;
     res.status(statusCode).json({
         status: 'success',
-        token: accessToken, // Можно не отправлять токен в теле ответа, если он уже в куках
-        user: { id: user.id, username: user.username }
+        token: accessToken,
+        user: safeUser
     });
 };
 
